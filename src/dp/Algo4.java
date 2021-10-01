458140abe6ab60051f92f1adb816825f16e6f701
shryu <jzxcvbn321@gmail.com>
2021-10-01 19:49:15 +0900

    feat : dp 4번 문제풀이

--- src/dp/Algo4.java
+++ src/dp/Algo4.java
@@ -1,2 +1,46 @@
-package dp;public class Algo4 {
+package dp;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Algo4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        int[][] array = new int[number][3];
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < 3; j++) {
+                array[i][j] = in.nextInt();
+            }
+        }
+        System.out.println(solution(array));
+    }
+
+    private static int solution(int[][] array) {
+        Arrays.sort(array, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o2[0] - o1[0];
+            }
+        });
+
+        int[] dynamic = new int[array.length];
+        dynamic[0] = array[0][1];
+        int answer = dynamic[0];
+
+        for (int i = 1; i < array.length; i++) {
+            int max = 0;
+            for (int j = i-1; j >= 0; j--) {
+                if (array[i][0] < array[j][0] && array[i][2] < array[j][2] && dynamic[j] > max) {
+                    max = dynamic[j];
+                }
+            }
+            dynamic[i] = max + array[i][1];
+            answer = Math.max(answer, dynamic[i]);
+        }
+
+        return answer;
+    }
 }
