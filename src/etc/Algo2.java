40ba38ea07e0b9ba46f1dbbcf0798b70af7208f5
shryu <jzxcvbn321@gmail.com>
2021-10-24 22:29:52 +0900

    feat : 오늘 테스트

---
+++ src/etc/Algo2.java
@@ -0,0 +1,17 @@
+package etc;
+
+import java.util.Scanner;
+
+public class Algo2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        System.out.println(solution(number));
+    }
+
+    private static int solution(int number) {
+        return 0;
+    }
+
+
+}
